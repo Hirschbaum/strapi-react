5d79ad4161c47c6a8402c26bbaeb23d8d7a9a40b
adrienn timko <adrienn.timko@gmail.com>
2021-01-10 17:37:56 +0100

    category names rendered as well

--- src/Webshop.js
+++ src/Webshop.js
@@ -4,10 +4,33 @@ import Navigation from "./Navigation";
 
 function Webshop() {
   const [products, setProducts] = useState(null);
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    getCategories();
+  });
 
   useEffect(() => {
     getProducts();
   }, []);
+
+  function getCategories() {
+    axios
+      .get("http://localhost:1337/categories")
+      .then((response) => {
+        console.log(response);
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.log(
+          "An error occurred while getting categories from strapi",
+          error
+        );
+      });
+    if (!categories) {
+      return <div></div>;
+    }
+  }
 
   function getProducts() {
     axios
@@ -40,18 +63,29 @@ function Webshop() {
       });
   }
 
+  function renderCategoryNames() {
+    return categories.map((category) => {
+      const { name, published_at } = category;
+      console.log(name);
+      return <li key={published_at}>{name}</li>;
+    });
+  }
+
   function renderProducts() {
     //console.log(products);
     return products.map((product) => {
-      const { id, photo, title, colors, price, favorite } = product;
+      const { id, photo, title, colors, price, favorite, categories } = product;
       const ending = photo.formats.medium.url;
       const colorsArray = colors
         .filter((obj) => obj.oneColor)
         .map((obj) => obj.oneColor)
         .join(" ");
 
+      const choosenCategory = categories[0].name;
+      //console.log(choosenCategory); works
+
       return (
-        <div key={id} className="product-card">
+        <div key={id} className="product-card" dataCategory={choosenCategory}>
           <img
             src={"//localhost:1337" + ending}
             alt={title}
@@ -81,7 +115,12 @@ function Webshop() {
   return (
     <div className="App">
       <Navigation />
-
+      <div>
+        <ul>
+          <li>all</li>
+          {renderCategoryNames()}
+        </ul>
+      </div>
       <div className="product-container" style={{ marginTop: "3%" }}>
         {renderProducts()}
       </div>
